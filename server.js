37a124054db70c2d05436a112600f6ec1a6d63e2
darnocer <darnocer@gmail.com>
2020-07-10 15:32:45 -0600

    handlebars prototype access

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ var express = require("express");
 var GitHubStrategy = require("passport-github").Strategy;
 var passport = require("passport");
 const db = require("./models");
+
 var PORT = process.env.PORT || 3000;
+
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Set Handlebars.
+var Handlebars = require("handlebars");
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype->access");
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
@@ -36,7 +50,6 @@ passport.use(
 app.get("/auth/github", passport.authenticate("github"));
 
 // Start our server so that it can begin listening to client requests.
-
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
