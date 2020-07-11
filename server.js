86bcdae7757a54f5b3971354d4b301079268a482
darnocer <darnocer@gmail.com>
2020-07-10 23:15:58 -0600

    logo html

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-var express = require("express");
-var GitHubStrategy = require("passport-github").Strategy;
-var passport = require("passport");
+// DEPENDENCIES
+const express = require("express");
+const GitHubStrategy = require("passport-github").Strategy;
+const passport = require("passport");
 const db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+// Set PORT
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+// Set Express App
+const app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var Handlebars = require("handlebars");
-var exphbs = require("express-handlebars");
+// Set Handlebars configuration
+const Handlebars = require("handlebars");
+const exphbs = require("express-handlebars");
+// used to bypass error https://handlebarsjs.com/api-reference/runtime-options.html#options-to-control-prototype-access
 const {
   allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
@@ -29,9 +33,9 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./routes/api-routes.js")(app);
-// var htmlroutes = require("./routes/html-routes.js")(app);
+const routes = require("./routes/api-routes.js")(app);
 
+// Authentication
 passport.use(
   new GitHubStrategy(
     {
