2cdf78c5daa1984f672860cf6cfc3d04d8234eaa
darnocer <darnocer@gmail.com>
2020-07-11 12:43:51 -0600

    stashing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@ const db = require("../models");
 module.exports = function (app) {
   app.get("/", (req, res) => {
     db.Project.findAll().then((data) => {
+      // res.json(data);
       res.render("index", { projects: data });
     });
   });
