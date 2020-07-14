3c1120dfec782dfd59451bb7ab1d1cc8b32df3d0
darnocer <darnocer@gmail.com>
2020-07-13 19:59:00 -0600

    rendering avatar and username

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,10 +3,9 @@ const db = require("../models");
 module.exports = function (app) {
   app.get("/", (req, res) => {
     db.Project.findAll({
+      raw: true,
       include: [db.User],
     }).then((data) => {
-      console.log(data);
-
       res.render("index", { projects: data });
     });
   });
