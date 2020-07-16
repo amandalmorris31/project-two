5609d4452b9d005e1b9279e236136878929e7b6d
darnocer <darnocer@gmail.com>
2020-07-16 05:32:33 -0600

    comments and console.logs

--- models/index.js
+++ models/index.js
@@ -9,6 +9,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
+const mysql = require("mysql2");
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
